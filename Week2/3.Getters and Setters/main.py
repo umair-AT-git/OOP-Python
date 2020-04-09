7c838e832aea6fd5cc9e16af99d5f599e7171e7d
unknown <umairisnow@gmail.com>
2020-04-09 17:35:45 +0530

    Implementing Getters and Setters

---
+++ Week2/3.Getters and Setters/main.py
@@ -0,0 +1,10 @@
+from room import Room
+
+#instatntiating (creating) an object
+
+kitchen = Room("Kitchen")
+
+#finding out what we learnt up to now
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+# kitchen.get_description()
+kitchen.describe()

d3d0995d3c944aa5313be053760f189996759dae
unknown <umairisnow@gmail.com>
2020-04-09 17:38:21 +0530

    main file updated to link rooms

---
+++ Week2/4.Linking Rooms/main.py
@@ -0,0 +1,21 @@
+from room import Room
+
+#instatntiating (creating) an object
+
+kitchen = Room("Kitchen")
+
+#finding out what we learnt up to now
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+# kitchen.get_description()
+kitchen.describe()
+
+dining_hall = Room("Dining_hall")
+dining_hall.set_description("Where gosts have their meals")
+
+ballroom = Room("Ballroom")
+ballroom.set_description("Where zombies have their parties")
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")

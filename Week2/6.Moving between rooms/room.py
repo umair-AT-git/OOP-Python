85c511514ddef4da0509b59cfe3716d8e345fc0c
unknown <umairisnow@gmail.com>
2020-04-09 17:45:07 +0530

    Room class file updated to move between rooms

---
+++ Week2/6.Moving between rooms/room.py
@@ -0,0 +1,46 @@
+class Room():
+	"""docstring for Room"""
+	"""Developing a constructor to initialize object"""
+	def __init__(self, room_name):
+
+		self.name = room_name
+		self.description = None
+		self.linked_rooms = {}
+
+	# setters and getters for the attributes
+
+	def get_description(self):
+		return self.description
+
+	def set_description(self, room_description):
+		self.description = room_description
+		
+	def get_room_name(self):
+		return self.name
+
+	def set_room_name(self, room_name):
+		self.name = room_name
+
+	def describe(self):
+		print (self.description)
+
+	#Adding linking room method
+	def link_room(self, room_to_link, direction):
+		self.linked_rooms[direction] = room_to_link
+
+	def get_details(self):
+		print(self.name)
+		print("===============")
+		print(self.description)
+		for direction in self.linked_rooms:
+			room = self.linked_rooms[direction]
+			print("The "+ room.get_room_name() +" is " + direction)
+
+
+	# Moving between rooms
+	def move(self, direction):
+		if direction in self.linked_rooms:
+			return self.linked_rooms[direction]
+		else:
+			print("You cant go that way!")
+			return(self)

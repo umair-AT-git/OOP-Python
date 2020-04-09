497311f45e28da50712c30def0fad7527f5ff6a7
unknown <umairisnow@gmail.com>
2020-04-09 17:34:06 +0530

    Room Class updated

---
+++ Week2/2.Instantiating an object/room.py
@@ -0,0 +1,8 @@
+class Room():
+	"""docstring for Room"""
+	"""Developing a constructor to initialize object"""
+	def __init__(self, room_name):
+
+		self.name = room_name
+		self.description = None
+

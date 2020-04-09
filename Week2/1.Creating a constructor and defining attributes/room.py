9255af08f52e8f711ecceb2cc7f683085b1db34e
unknown <umairisnow@gmail.com>
2020-04-09 17:31:25 +0530

    Room Class initiated

---
+++ Week2/1.Creating a constructor and defining attributes/room.py
@@ -0,0 +1,9 @@
+class Room():
+	"""docstring for Room"""
+	"""Developing a constructor to initialize object"""
+	def __init__(self, room_name):
+
+		self.name = room_name
+		self.description = None
+
+	
